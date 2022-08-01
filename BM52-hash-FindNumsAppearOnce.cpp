cfa37566aa2fe88321ad9531e4368daaecfe48c0
hou <919863463@qq.com>
2022-08-01 18:46:01 +0800

    hash +BM52 +BM53

--- BM52-hash-FindNumsAppearOnce.cpp
+++ BM52-hash-FindNumsAppearOnce.cpp
@@ -1,32 +1,28 @@
 // BM52 数组中只出现一次的两个数字
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 class Solution {
 public:
     vector<int> FindNumsAppearOnce(vector<int>& array) {
-        vector<int> res(2, 0);
-        int temp = 0;
-        //遍历数组得到a^b
-        for(int i = 0; i < array.size(); i++)
-            temp ^= array[i];
+        vector<int>res(2, 0);
+        int tmp = 0;
+        for(int i=0; i<array.size(); i++) {
+            tmp ^= array[i];
+        }
         int k = 1;
-        //找到两个数不相同的第一位
-        while((k & temp) == 0)
+        while((k & tmp)==0)
             k <<= 1;
-        for(int i = 0; i < array.size(); i++){
-            //遍历数组，对每个数分类
-            if((k & array[i]) == 0)
+        for(int i=0; i<array.size(); i++) {
+            if ((k & array[i])==0)
                 res[0] ^= array[i];
             else
                 res[1] ^= array[i];
         }
-        //整理次序
-        if(res[0] < res[1])
-            return res;
-        else
-            return {res[1], res[0]};
+        sort(res.begin(), res.end());
+        return res;
     }
 };
--- BM53-hash-minNumberDisappeared.cpp
+++ BM53-hash-minNumberDisappeared.cpp
@@ -1,6 +1,7 @@
 // BM53 缺失的第一个正整数
 #include <iostream>
 #include <vector>
+#include <math.h>
 
 using namespace std;
 class Solution{
@@ -8,12 +9,18 @@ public:
     int minNumberDisappeared(vector<int> &nums) {
         int n = nums.size();
         for(int i=0; i<n; i++) {
-            // 此位应该出现 i+1
-            if (nums[n]!=i+1) {
-                while() {
-                    
-                }
+            if (nums[i]<=0)
+                nums[i] = n+1;
+        }
+        for(int i=0; i<n; i++) {
+            if (nums[i]<n+1 && nums[ abs(nums[i])-1 ]>0) {
+                nums[ abs(nums[i])-1 ] = -nums[ abs(nums[i])-1 ];
             }
         }
+        for(int i=0; i<n; i++) {
+            if (nums[i]>0 || nums[i]==n+1)
+                return i+1;
+        }
+        return n+1;
     }
 };
